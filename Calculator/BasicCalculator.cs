359d081650c82fbb3aaaf2867b8e17472f24e90a
Joe <joseph.varrasso@mohawkcollege.ca>
2022-11-07 16:46:41 -0500

    Added tests for the calculator using the Test Driven Development (TDD) approach.

--- Calculator/BasicCalculator.cs
+++ Calculator/BasicCalculator.cs
@@ -15,10 +15,26 @@ namespace Calculator
 
         public int Divide(int dividend, int divisor)
         {
+            // RED STAGE
+            // throw new NotImplementedException();
+
+            //REFACTOR - argument checks
             if (divisor == 0)
                 throw new ArgumentException("Divisor argument error - Can't divide by zero");
+
+            // GREEN STAGE - minimum amount of code for test to pass
             return dividend / divisor;
         }
 
+        public int Multiply(int v1, int v2)
+        {
+            // RED STATE
+            // throw new NotImplementedException();
+
+            // GREEN STATE
+            return v1 * v2;
+
+            // REFACTOR STATE - SKIP AS THERE IS NOT ELSE WE CAN DO
+        }
     }
 }
--- CalculatorUnitTests/CalculatorTests.cs
+++ CalculatorUnitTests/CalculatorTests.cs
@@ -30,5 +30,31 @@ namespace CalculatorUnitTests
             // Act + Assert
             Assert.ThrowsException<ArgumentException>(() => myCalculator.Divide(10, 0), "Divide by zero did not cause an ArgumentException");
         }
+
+        [TestMethod]
+        public void DivideByOneTest()
+        {
+            // Arrange
+            BasicCalculator myCalculator = new BasicCalculator();
+
+            // Act
+            int result = myCalculator.Divide(1, 1);
+
+            // Assert
+            Assert.IsTrue(result == 1);
+        }
+
+        [TestMethod]
+        public void MultiplyTest()
+        {
+            // Arrange
+            BasicCalculator myCalculator = new BasicCalculator();
+
+            // Act
+            int result = myCalculator.Multiply(10, 1);
+
+            // Assert
+            Assert.IsTrue(result == 10);
+        }
     }
 }
