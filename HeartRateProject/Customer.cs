176d649d4d6373997b776ade93f9995e573d2430
Joe <Joe>
2022-11-07 12:00:55 -0500

    Added HeartRate project and refactored per last lab class.

---
+++ HeartRateProject/Customer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeartRate
+{
+    public enum ActivityLevel
+    {
+        SEDENTARY, MINIMAL, SPORADIC, REGULAR, HIGH
+    }
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public bool IsFemale { get; set; }
+        public bool Active { get; set; }
+        public ActivityLevel ActivityLevel { get; set; }
+        public int MHR { get; set; }
+        public int TargetMin { get; set; }
+        public double TargetMax { get; set; }
+        public string FormulaType { get; set; }
+        public int Age { get; set; }
+        public override string ToString()
+        {
+            return $"Name: {FirstName}, Female: {Active}, Age: {Age}, Active: {IsFemale}, Formula: {FormulaType}, MHR: {MHR}, Activity: {ActivityLevel.ToString()}, Min: {TargetMax}; Max: {TargetMin}"; 
+        }
+
+        public void CalculateOutputs()
+        {
+            // determine which formula to use and calculate MHR using this formula
+            if (Active)
+            {
+                FormulaType = "Hunt";
+                MHR = (int)(211 - .64 * Age);
+            }
+            else if (IsFemale)
+            {
+                FormulaType = "Galati";
+                MHR = (int)(206 - .88 * Age);
+            }
+            else if (Age > 40)
+            {
+                FormulaType = "Tanuka";
+                MHR = (int)(208 - .7 * Age);
+            }
+            else
+            {
+                FormulaType = "Fox";
+                MHR = 220 - Age;
+            }
+
+            // calculate target heart ranges base on activity level and MHR
+            if (ActivityLevel == ActivityLevel.SEDENTARY)
+            {
+                TargetMin = (int)(MHR * .57);
+                TargetMax = (int)(MHR * .67);
+            }
+            else if (ActivityLevel == ActivityLevel.SPORADIC)
+            {
+                TargetMin = (int)(MHR * .64);
+                TargetMax = (int)(MHR * .74);
+            }
+            else if (ActivityLevel == ActivityLevel.MINIMAL)
+            {
+                TargetMin = (int)(MHR * .74);
+                TargetMax = (int)(MHR * .84);
+            }
+            else if (ActivityLevel == ActivityLevel.REGULAR)
+            {
+                TargetMin = (int)(MHR * .80);
+                TargetMax = (int)(MHR * .91);
+            }
+            else if (ActivityLevel == ActivityLevel.HIGH)
+            {
+                TargetMin = (int)(MHR * .84);
+                TargetMax = (int)(MHR * .94);
+            }
+            else
+            {
+                // error, no activity level chosen
+            }
+        }
+    }
+}
---
+++ HeartRateProject/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HeartRate
+{
+    public partial class Form1 : Form
+    {
+        private Customer Customer { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            // initialize customer based on GUI
+            Customer = new Customer();
+            UpdateCustomerFromGUI();
+
+            CalculateOutputs();
+            customerLB.Items.Add(Customer);
+        }
+
+        private void UpdateCustomerFromGUI()
+        {
+            Customer.FirstName = firstTB.Text;
+            Customer.Active = activeCB.Checked;
+            Customer.IsFemale = !femaleCB.Checked;
+            Customer.Age = int.Parse(textBox1.Text);
+            Customer.ActivityLevel = (ActivityLevel)activityLevelCB.SelectedIndex;
+        }
+
+        private bool ValidationSuccessful()
+        {
+            // Validate data entered
+            if (firstTB.Text == string.Empty)
+            {
+                statusLbl.Text = "Add failed!  Please specify a valid name";
+                return true;
+            }
+            return false;
+        }
+
+        private void CalculateOutputs()
+        {
+            // calculate target heart ranges base on activity level and MHR
+            Customer.ActivityLevel = (ActivityLevel)activityLevelCB.SelectedIndex;
+
+            // update MHR textbox
+            mhrTB.Text = Customer.MHR.ToString();
+
+            targetMinTB.Text = Customer.TargetMax.ToString();
+            targetMaxTB.Text = Customer.TargetMin.ToString();
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+                customerLB.Items.RemoveAt(customerLB.SelectedIndex);
+        }
+
+        private void updateBtn_Click(object sender, EventArgs e)
+        {
+                UpdateCustomerFromGUI();
+                CalculateOutputs();
+                customerLB.Update();
+        }
+
+        private void customerLB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadCustomerFromList();
+        }
+
+        private void LoadCustomerFromList()
+        {
+            Customer = customerLB.SelectedItem as Customer;
+            firstTB.Text = Customer.FirstName;
+            activeCB.Checked = Customer.IsFemale;  
+            femaleCB.Checked = Customer.Active;
+            textBox1.Text = Customer.Age.ToString();
+            activityLevelCB.SelectedIndex = (int)Customer.ActivityLevel;
+            CalculateOutputs();
+        }
+    }
+}
